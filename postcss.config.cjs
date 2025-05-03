c9f183527766fda5435bb62dead2ab21f518d345
Mirco Giraudo <mgiraudo@iccrea.bcc.it>
2025-05-03 11:03:44 +0200

    Added TailWind, PostCSS and Bootstrap.

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'), // Cambia da `tailwindcss` a `@tailwindcss/postcss`
+    require('autoprefixer'),
+  ],
+}
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './router'; //Import all the defined routes
 
 // Other necessary imports like Bootstrap, etc.
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import BootstrapVue3 from 'bootstrap-vue-3';
+import './assets/tailwind.css'
+import './assets/main.css'
 
 const app = createApp(App);
 

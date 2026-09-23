6ccc75d309ea26c098972485267e913f56fd6d25
Mirco Giraudo <mrcgiraudo@gmail.com>
2026-09-23 21:01:09 +0200

    Set base path for gh-pages, cleaned index.html and removed bootstrap-vue.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mircogiraudo-cv/',
   css: {
     postcss: './postcss.config.cjs',
   },
